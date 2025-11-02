a7ff5b0e5cb0d49bd10883d56942542213646e6f
preetkakdiya <preetkakdiya@gmail.com>
2025-11-02 12:11:00 +0530

    Initial commit: full-stack chat app with K8s setup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001"   // when running locally
+      : "http://backend:5001",    // when running inside Kubernetes
   withCredentials: true,
 });
